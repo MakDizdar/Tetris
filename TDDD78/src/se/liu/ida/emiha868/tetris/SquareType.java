d8138ee2135bf0fbda910a4e873083631b0b7513
Uzor <emiha868@liu.se>
2016-04-16 17:14:32 +0200

    Uploading the project with javadocs

--- TDDD78/src/se/liu/ida/emiha868/tetris/SquareType.java
+++
@@ -1,6 +0,0 @@
-package se.liu.ida.emiha868.tetris;
-
-public enum SquareType
-{
-    I,J,L,O,S,T,Z,EMPTY,OUTSIDE
-}
--- TDDD78/src/se/liu/ida/emiha868/tetris/gui/BoardListener.java
+++
@@ -1,6 +0,0 @@
-package se.liu.ida.emiha868.tetris.gui;
-
-public interface BoardListener
-{
-    public void boardChanged();
-}
